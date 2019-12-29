cfbe6ce70346285df1903fb0b5f7752f26e5b12b
Tomas Husak <tomikhusak@seznam.cz>
2019-12-29 12:41:36 +0100

    Resizable array

---
+++ Semestral_project_app/SATsolver/resizable_array.h
@@ -0,0 +1,43 @@
+#pragma once
+#include <vector>
+#include <memory>
+
+template<typename T> class resizable_array
+{
+public:
+	class iterator {
+	public:
+		iterator(const iterator& it) : ar_(it.ar_), position_(it.position_) {}
+		iterator(const resizable_array& ar, size_t position) : ar_(ar), position_(position) {}
+		T& operator*() { return ar_.at(position_); }
+		bool operator!=(const iterator& it) { return position_ != it.position_; }
+		iterator operator++() { position_++; return *this; }
+	private:
+		const resizable_array<T>& ar_;
+		size_t position_{ 0 };
+	};
+	resizable_array(size_t chunk_size = 100) : chunk(chunk_size) {}
+	void push_back(const T& x);
+	T& operator[](size_t index) const { return references[index / chunk][index % chunk]; }
+	T& at(size_t index) const { if (index < size) { return (*this)[index]; } else throw; }
+	iterator begin() { return iterator(*this, 0); }
+	iterator end() { return iterator(*this, size); }
+private:
+	std::vector<std::unique_ptr<T[]>> references;
+	void resize(); // Creates new space for item
+	size_t chunk;
+	size_t size{ 0 };
+};
+
+template<typename T> void resizable_array<T>::push_back(const T& x)
+{
+	resize();
+	references[references.size() - 1][size++ % chunk] = x;
+}
+
+template<typename T> void resizable_array<T>::resize()
+{
+	if (size + 1 > references.size()* chunk)
+		references.push_back(std::make_unique<T[]>(chunk));
+}
+
