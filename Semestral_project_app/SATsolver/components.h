eb5905eb25ddf1432f18d41923e4a731eecbb51d
Tomas Husak <tomikhusak@seznam.cz>
2020-01-02 22:05:46 +0100

    documentation components

--- Semestral_project_app/SATsolver/components.h
+++ Semestral_project_app/SATsolver/components.h
@@ -4,7 +4,10 @@
 #include <string>
 #include "resizable_array.h"
 
-// Assignments units for literals
+/**
+ * @brief Represents variable and assignment.
+ * 
+ */
 class variable {
 	public:
 		variable() : id_(0), assignment_(2) {}
@@ -15,17 +18,28 @@ class variable {
 		void set_assignment(char assignment) { assignment_ = assignment; }
 	private:
 		unsigned int id_;
-		char assignment_; // 0 - false, 1 - true, something else - undefined
+		/**
+		 * @brief Values: 0 - false, 1 = true, something else - undefined
+		 * 
+		 */
+		char assignment_;
 };
 
 #pragma region Observers
 
+/**
+ * @brief Class helper for identifing positive and negative literal observer.
+ * 
+ */
 enum lTYPE {
 	positive,
 	negative
 };
 
-// Abstract class of representants of variables(literals)
+/**
+ * @brief Abstract class representants literals.
+ * 
+ */
 class literal_observer {
 	public:
 		literal_observer(variable& var) : var_(var) {}
@@ -37,6 +51,10 @@ class literal_observer {
 		virtual std::shared_ptr<literal_observer> clone() const = 0;
 		virtual void set_assignment(char assignment) = 0;
 	protected:
+	/**
+	 * @brief Instance of variable, which represents assignment of literal.
+	 * 
+	 */
 		variable& var_;
 };
 
@@ -52,7 +70,17 @@ class positive_literal_observer : public literal_observer {
 class negative_literal_observer : public literal_observer {
 	public:
 		negative_literal_observer(variable& var) : literal_observer(var) {}
-		char get_assignment() const override; // Gets negetion of variable assignment 
+		/**
+		 * @brief Get the assignment object due to negative literal, so it is negated. 
+		 * 
+		 * @return char as assignment
+		 */
+		char get_assignment() const override;
+		/**
+		 * @brief Set the assignment object due to negative literal, so it is negated.
+		 * 
+		 * @param assignment Method interprets it as negation of asignment.
+		 */
 		virtual void set_assignment(char assignment) override;
 		virtual std::shared_ptr<literal_observer> clone() const override { return std::make_shared<negative_literal_observer>(*this); };
 		virtual lTYPE get_type() const { return lTYPE(negative); };
@@ -64,6 +92,10 @@ class clause {
 	public:
 		literal_observer& operator[](size_t index) const { return *literals_[index]; }
 		literal_observer& at(size_t index) const { if (index < literals_.size()) { return (*this)[index]; } else throw; }
+		/**
+		 * @brief Iterator throw literals in clause.
+		 * 
+		 */
 		class iterator {
 		public:
 			iterator(const iterator& it) : c_(it.c_), position_(it.position_) {}
@@ -82,7 +114,6 @@ class clause {
 		bool constains(const literal_observer& literal) const; 
 		void add_literal(const literal_observer& literal) { literals_.push_back(literal.clone()); }
 	private:
-
 		class ftor_is_item {
 		public:
 			ftor_is_item(const literal_observer& literal) :literal_(literal){}
@@ -98,6 +129,10 @@ class cnf_formula {
 	public:
 		clause& operator[](size_t index) const { return clauses_[index]; }
 		clause& at(size_t index) const { if (index < clauses_.size()) { return (*this)[index]; } else throw; }
+		/**
+		 * @brief Iterator throw clauses in cnf formula.
+		 * 
+		 */
 		class clause_iterator {
 		public:
 			clause_iterator(const clause_iterator& it) : f_(it.f_), position_(it.position_) {}
@@ -112,44 +147,88 @@ class cnf_formula {
 		clause_iterator begin() const { return clause_iterator(*this, 0); }
 		clause_iterator end() const { return clause_iterator(*this, clauses_.size()); }
 
+		/**
+		 * @brief Loads formula to intern containers.
+		 * 
+		 * @param input Dimacs format of cnf formula.
+		 */
 		void load_formula(std::istream& input);
 
 		void print_formula(std::ostream& output) const;
 
+		/**
+		 * @brief Gets count of variables constained in cnf formula.
+		 * 
+		 * @return size_t 
+		 */
 		size_t count_of_variables() const {return variables_.size();}
 
-		// if clause is unit, returns unsigned observer, else NULL
+		/**
+		 * @brief Checks, if clause is unit clause. 
+		 * 
+		 * @return literal_observer* returns NULL, if not, else literal, which is unassigned.
+		 */
 		literal_observer* is_unit_clause(const clause&) const;
 
-		// check, if there is conflict clause
+		/**
+		 * @brief Seeks, if formula contains conflict clause.
+		 * 
+		 * @return clause* returns NULL, if not, else clause, which is conflicting.
+		 */
 		clause* seek_conflict() const; 
 
-		// if clause is conflict, returns true, else NULL
+		/**
+		 * @brief Checks, if clause is conflict clause. 
+		 * 
+		 */
 		bool is_conflict_clause(const clause& c) const;
 
-		// if no vars is ubsigned, returns NULL
+		/**
+		 * @brief Finds first unsigned var.
+		 * 
+		 * @return literal_observer* NULL if no vars is unassigned 
+		 */
 		literal_observer* find_first_unsigned_var() const;
 
 		const variable& get_variable( int id) const {return  variables_[id-1];}
 
-		// positive == true -> returns positives, else negative
+		/**
+		 * @brief Get the observer object.
+		 * 
+		 * @param variable_id 
+		 * @param positive true returns positive literal, else negative
+		 * @return const literal_observer& 
+		 */
 		const literal_observer& get_observer(int variable_id, bool positive) const ;
 
 		void add_clause(const clause& c) {clauses_.push_back(c) ;}
 	private:
-
+		/**
+		 * @brief Checks of str_to_comp constains prefix. 
+		 * 
+		 */
 		bool starts_with(const std::string& str_to_comp, const std::string& prefix) const;
+
+		/**
+		 * @brief Parse clause from line in dimacs format.
+		 * 
+		 */
 		clause read_clause(const std::string& line) const;
 
 		std::vector<variable> variables_;
 
-		// negative are on odd position, positive on even positions
+		/**
+		 * @brief Negative literals are on odd position, positives on even positions.
+		 * 
+		 */
 		std::vector<std::shared_ptr<literal_observer>> observers_; 
 
-		// resizable_array because iterators, references and pointers are same
+		/**
+		 * @brief iterators, references and pointers safe usage
+		 * 
+		 */
 		resizable_array<clause> clauses_; 
 
-		// Constants of dimacs cnf loader
 		const char comment_ = 'c';
 		const std::string paragrapf_ = "p cnf";
 };
